c2560608e8da3f4ef2b97d402f863a6d562178c0
zlochmateusz <zlochmateusz@gmail.com>
2017-07-03 15:32:41 +0200

    string reverse

--- src/stringReverse.java
+++ src/stringReverse.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
+class Apps {
+
+}
 public class stringReverse {
+    public static void main (String[] args) {
+        String text;
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Podaj text do odwrócenia");
+
+        text = odczyt.nextLine();
+
+        String odw = new StringBuilder(text).reverse().toString();
+
+
+        System.out.println("Odwórcony text to: " + odw);
+
+
+    }
 }
